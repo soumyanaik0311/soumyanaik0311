811eab2aa049753a4f44259cbd9c332d29d8374c
Soumya Ranjan Naik <soumyasonunaik.rkl@gmail.com>
2022-01-28 13:03:24 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class hello 
+{
+    public static void main(String args[]) 
+    {
+
+        int r, n, rev = 0, fd, ld, s;
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number:");
+            n = sc.nextInt();
+        }
+        ld = n % 10;
+
+        while (n > 0) {
+            r = n % 10;
+            rev = rev * 10 + r;
+            n = n / 10;
+        }
+
+        fd = rev % 10;
+        s = fd + ld;
+
+        System.out.println("Sum of first and last digits:" + s);
+
+    }
+}
